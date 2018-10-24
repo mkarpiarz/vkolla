0c02f48866998536f31c29e5a9e6fc6d8cec5950
Mariusz Karpiarz <mariusz@karpiarz.net>
2018-10-24 02:20:42 +0100

    Print out server name, addresses and metadata

--- generate_inventory.py
+++ generate_inventory.py
@@ -2,6 +2,7 @@ import os
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient import client as keystone_client
+from novaclient import client as nova_client
 
 
 class Credentials:
@@ -24,10 +25,27 @@ class Credentials:
         sess = session.Session(auth=auth)
         return keystone_client.Client(version=3, session=sess)
 
+    def get_session(self):
+        if self.client:
+            return self.client.session
+
+
+class Servers(object):
+    def __init__(self, creds):
+        self.client = nova_client.Client(version=2,
+                                         session=creds.get_session())
+
+    def get_servers(self):
+        return self.client.servers.list(detailed=True)
+
 
 def main():
     creds = Credentials()
-    print(creds.client)
+    servers = Servers(creds)
+    for server in servers.get_servers():
+        print(server.name)
+        print(server.addresses)
+        print(server.metadata)
 
 
 if __name__ == "__main__":
